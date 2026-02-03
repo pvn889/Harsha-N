974721f9c7671b9fc02c73a9f0296ac74c519e1e
pvn889 <pvnchamp@gmail.com>
2026-02-02 22:11:13 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 100);
+
+  noBtn.style.position = "absolute";
+  noBtn.style.left = x + "px";
+  noBtn.style.top = y + "px";
+});
+
+function goYes() {
+  window.location.href = "celebrate.html";
+}
 
